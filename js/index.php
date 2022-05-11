b8b569e0e5b8154a1f7da8988ab007eb8ef87b01
samsu22 <93289865+samsu22@users.noreply.github.com>
2022-05-11 20:22:43 +0300

    Add files via upload

---
+++ js/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Youtube - Games</title>
+
+	<!-- Global stylesheets -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+	<link href="global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/layout.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/components.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/colors.min.css" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script src="global_assets/js/main/jquery.min.js"></script>
+	<script src="global_assets/js/main/bootstrap.bundle.min.js"></script>
+	<script src="global_assets/js/plugins/loaders/blockui.min.js"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script src="assets/js/app.js"></script>
+	<!-- /theme JS files -->
+
+</head>
+
+<body>
+
+	
+
+			<!-- Content area -->
+			<div class="content">
+<div class="navbar navbar-dark navbar-expand-lg" style="z-index: 27;">
+		
+								<div class="d-xl-none">
+									<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-navigation">
+										<i class="icon-menu"></i>
+									</button>
+								</div>
+
+								<div class="navbar-collapse collapse" id="navbar-navigation">
+									<ul class="navbar-nav">
+										<li class="nav-item"><a href="#" class="navbar-nav-link active">PUBG MOBILE</a></li>
+										<li class="nav-item"><a href="#" class="navbar-nav-link active"></a></li>
+											<li class="nav-item"><a href="#" class="navbar-nav-link active">Game 1 </a></li>
+									</ul>
+
+							
+								</div>
+							</div>
+
+				<div class="row">
+
+
+
+
+	<div class="col-xl-3 col-sm-6">
+						<div class="card">
+							<div class="card-img-actions">
+								<img class="card-img-top img-fluid" src="global_assets/images/placeholders/free.png" alt="">
+								<div class="card-img-actions-overlay card-img-top">
+									<a href="link youtube" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round ml-2">
+										<i class="icon-link"></i>
+									</a>
+								</div>
+							</div>
+
+					    	<div class="card-body text-center">
+					    		<h6 class="font-weight-semibold mb-0"></h6>
+					    		<span class="d-block text-muted"></span>
+
+				    			<div class="list-icons list-icons-extended mt-3">
+			                    	<a href="#" class="list-icons-item" data-popup="tooltip" title="Youtube" data-container="body"><i class="icon-youtube"></i></a>
+		                    	</div>
+					    	</div>
+				    	</div>
+					</div> 
+								
+								
+							<div class="col-xl-3 col-sm-6">
+						<div class="card">
+							<div class="card-img-actions">
+								<img class="card-img-top img-fluid" src="global_assets/images/placeholders/free.png" alt="">
+								<div class="card-img-actions-overlay card-img-top">
+									<a href="link youtube" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round ml-2">
+										<i class="icon-link"></i>
+									</a>
+								</div>
+							</div>
+
+					    	<div class="card-body text-center">
+					    		<h6 class="font-weight-semibold mb-0"></h6>
+					    		<span class="d-block text-muted"></span>
+
+				    			<div class="list-icons list-icons-extended mt-3">
+			                    	<a href="#" class="list-icons-item" data-popup="tooltip" title="Youtube" data-container="body"><i class="icon-youtube"></i></a>
+		                    	</div>
+					    	</div>
+				    	</div>
+					</div> 		
+								
+								
+								
+								
+			
+			
+				<div class="col-xl-3 col-sm-6">
+						<div class="card">
+							<div class="card-img-actions">
+								<img class="card-img-top img-fluid" src="global_assets/images/placeholders/free.png" alt="">
+								<div class="card-img-actions-overlay card-img-top">
+									<a href="link youtube" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round ml-2">
+										<i class="icon-link"></i>
+									</a>
+								</div>
+							</div>
+
+					    	<div class="card-body text-center">
+					    		<h6 class="font-weight-semibold mb-0"></h6>
+					    		<span class="d-block text-muted"></span>
+
+				    			<div class="list-icons list-icons-extended mt-3">
+			                    	<a href="#" class="list-icons-item" data-popup="tooltip" title="Youtube" data-container="body"><i class="icon-youtube"></i></a>
+		                    	</div>
+					    	</div>
+				    	</div>
+					</div> 
+					
+					
+					
+						<div class="col-xl-3 col-sm-6">
+						<div class="card">
+							<div class="card-img-actions">
+								<img class="card-img-top img-fluid" src="global_assets/images/placeholders/free.png" alt="">
+								<div class="card-img-actions-overlay card-img-top">
+									<a href="link youtube" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round ml-2">
+										<i class="icon-link"></i>
+									</a>
+								</div>
+							</div>
+
+					    	<div class="card-body text-center">
+					    		<h6 class="font-weight-semibold mb-0"></h6>
+					    		<span class="d-block text-muted"></span>
+
+				    			<div class="list-icons list-icons-extended mt-3">
+			                    	<a href="#" class="list-icons-item" data-popup="tooltip" title="Youtube" data-container="body"><i class="icon-youtube"></i></a>
+		                    	</div>
+					    	</div>
+				    	</div>
+					</div> 
+					
+					
+						<div class="col-xl-3 col-sm-6">
+						<div class="card">
+							<div class="card-img-actions">
+								<img class="card-img-top img-fluid" src="global_assets/images/placeholders/free.png" alt="">
+								<div class="card-img-actions-overlay card-img-top">
+									<a href="link youtube" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round ml-2">
+										<i class="icon-link"></i>
+									</a>
+								</div>
+							</div>
+
+					    	<div class="card-body text-center">
+					    		<h6 class="font-weight-semibold mb-0"></h6>
+					    		<span class="d-block text-muted"></span>
+
+				    			<div class="list-icons list-icons-extended mt-3">
+			                    	<a href="#" class="list-icons-item" data-popup="tooltip" title="Youtube" data-container="body"><i class="icon-youtube"></i></a>
+		                    	</div>
+					    	</div>
+				    	</div>
+					</div> 
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+				
+				</div>
+
+		
+
+			</div>
+			<!-- /content area -->
+
+
+			<!-- Footer -->
+			<div class="navbar navbar-expand-lg navbar-light">
+				<div class="text-center d-lg-none w-100">
+					<button type="button" class="navbar-toggler dropdown-toggle" data-toggle="collapse" data-target="#navbar-footer">
+						<i class="icon-unfold mr-2"></i>
+						Footer
+					</button>
+				</div>
+
+				<div class="navbar-collapse collapse" id="navbar-footer">
+					<span class="navbar-text">
+						&copy; 2022
+					</span>
+
+				</div>
+			</div>
+			<!-- /footer -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+</body>
+</html>
